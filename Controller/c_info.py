d4db0872f946503a9249d5c309e9f0a28ee63337
SiGae <sigae@protonmail.com>
2021-10-06 20:43:52 +0900

    연령대별 방문 수 구현

--- Controller/c_info.py
+++ Controller/c_info.py
@@ -72,12 +72,46 @@ def get_visit(db):
         .join(VisitOccurrence, VisitOccurrence.person_id == Person.person_id) \
         .group_by(Concept.concept_name) \
         .all()
-    print(gender_data)
+
+    query = """
+    SELECT
+        CASE
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth < 10
+                THEN '10이하'
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth BETWEEN 10 AND 19
+                THEN '10대'
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth BETWEEN 20 AND 29
+                THEN '20대'
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth BETWEEN 30 AND 39
+                THEN '30대'
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth BETWEEN 40 AND 49
+                THEN '40대'
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth BETWEEN 50 AND 59
+                THEN '50대'
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth BETWEEN 60 AND 69
+                THEN '60대'
+            WHEN (SELECT date_part('year', now()))- person.year_of_birth BETWEEN 70 AND 79
+                THEN '70대'
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth BETWEEN 80 AND 89
+                THEN '80대'
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth BETWEEN 90 AND 99
+                THEN '90대'
+            WHEN (SELECT date_part('year', now())) - person.year_of_birth > 99
+                THEN '90대 이상'
+            END AS age,
+        COUNT(*)
+    FROM de.visit_occurrence
+        JOIN person ON ( de.visit_occurrence.person_id = de.person.person_id )
+    GROUP BY
+        age
+    """
+    age_data = db.execute(query).all()
+
     response_data = {
         '방문 유형별 방문 수': visit_concept_data,
         '성별 방문 수 ': l_convert.do(gender_data),
         '인종별 방문 수': l_convert.do(race_data),
         '민족별 방문 수': l_convert.do(ethnicity_data),
-        '연령대 별 방문 수': ""
+        '연령대 별 방문 수': l_convert.do(age_data)
     }
     return response_data
